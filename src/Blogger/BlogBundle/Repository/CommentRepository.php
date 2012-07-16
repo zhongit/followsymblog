91559a1ed6fcfe07e8f9a4590081e3c4aa0b20ac
Zhong Chen <chen.zhong@rocketmail.com>
2012-07-16 08:10:40 +0200

    page templates issue: index images style further tasks: forms in pages, login system, database
    index image style fixed

--- src/Blogger/BlogBundle/Repository/CommentRepository.php
+++ src/Blogger/BlogBundle/Repository/CommentRepository.php
@@ -1,4 +1,5 @@
 <?php
+// src/Blogger/BlogBundle/Repository/CommentRepository.php
 
 namespace Blogger\BlogBundle\Repository;
 
@@ -12,4 +13,19 @@ use Doctrine\ORM\EntityRepository;
  */
 class CommentRepository extends EntityRepository
 {
+    public function getCommentsForBlog($blogId, $approved = true)
+    {
+        $qb = $this->createQueryBuilder('c')
+                   ->select('c')
+                   ->where('c.blog = :blog_id')
+                   ->addOrderBy('c.created')
+                   ->setParameter('blog_id', $blogId);
+
+        if (false === is_null($approved))
+            $qb->andWhere('c.approved = :approved')
+               ->setParameter('approved', $approved);
+
+        return $qb->getQuery()
+                  ->getResult();
+    }
 }
